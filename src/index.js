c9eb15bd28dcb4b7dd8bebdf6cd54215b154d0b1
maen <maen.1980@yahoo.com>
2022-03-25 03:05:23 +0300

    install linter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
+/* eslint-disable*/
 import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-   return element;
- }
-
- document.body.appendChild(component());
+}
