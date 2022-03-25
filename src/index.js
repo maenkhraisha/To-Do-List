d0f832d78bcfe01406679a60a94394f9a244a499
maen <maen.1980@yahoo.com>
2022-03-26 01:32:35 +0300

    check linter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import './style.css';
 import 'boxicons';
 import { getList } from './module/meme-object.js';
 import dom from './module/add-Dom-elem.js';
-import _ from './module/crud.js';
+import d from './module/crud.js';
+
+const domBtnInsert = document.getElementById('insert');
+const domBtnDelete = document.getElementById('btn-clear');
 
 getList().forEach((element) => {
   dom.creatElements(element.description, element.index);
+});
+
+domBtnInsert.addEventListener('click', () => {
+  d.btnInsert();
+});
+domBtnDelete.addEventListener('click', () => {
+  d.btnDelete();
 });
--- src/module/crud.js
+++ src/module/crud.js
@@ -1,23 +1,19 @@
 import { add, del, getLen } from './meme-object.js';
 import dom from './add-Dom-elem.js';
 
-const btnInsert = document.getElementById('insert');
 const inputText = document.getElementById('add-text');
-const btnDelete = document.getElementById('btn-clear');
 
-btnInsert.addEventListener('click', () => {
+const btnInsert = () => {
   add(inputText.value);
   inputText.value = '';
   dom.creatElements(inputText.value, getLen());
-});
+};
 
-btnDelete.addEventListener('click', () => {
+const btnDelete = () => {
   const checkboxlist = document.querySelectorAll('.checkbox');
   checkboxlist.forEach((element) => {
     if (element.checked === true) del(element.getAttribute('id'));
   });
-});
+};
 
-// checkbox.addEventListener('change', (event) => {
-//   event.target.value = 'select';
-// });
+export default { btnInsert, btnDelete };
