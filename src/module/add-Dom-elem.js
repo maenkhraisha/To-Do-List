e084de0325f9a9027563da2c1c622c74221ca446
maen <maen.1980@yahoo.com>
2022-03-28 16:06:37 +0300

    add dot icon to the list

--- src/module/add-Dom-elem.js
+++ src/module/add-Dom-elem.js
@@ -5,17 +5,23 @@ const creatElements = (desc, index) => {
   const elText = document.createElement('p');
   const elCheckBox = document.createElement('INPUT');
   const hLine = document.createElement('hr');
-  // contenteditable="true"
+  const elIcon = document.createElement('box-icon');
+
   elCheckBox.setAttribute('type', 'checkbox');
   elCheckBox.setAttribute('id', index);
   elCheckBox.className = 'checkbox';
   elCheckBox.className = 'checkbox';
   elText.innerText = desc;
+  elIcon.setAttribute('name', 'dots-vertical-rounded');
+  elIcon.className = 'li-edit-icon';
   elText.setAttribute('contenteditable', 'true');
 
   elLi.appendChild(elCheckBox);
   elLi.appendChild(elText);
+  elLi.appendChild(elIcon);
   elLi.appendChild(hLine);
+
+  // elLi.appendChild('<box-icon name=\'dots-vertical-rounded\'></box-icon>');
   parentUL.appendChild(elLi);
 };
 
